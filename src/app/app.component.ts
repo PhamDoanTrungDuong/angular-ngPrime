39625791d331b1ac1a6fb6ca7413dd8542a5aebe
Dương Phạm <pdtduong03@gmail.com>
2022-01-11 13:56:28 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Angualar12_ngPrime';
+
+export class AppComponent { 
+
+  public isLoading: any 
+  public cars: any 
+  public products:any
+  public responsiveOptions: any
+  items: MenuItem[] = [];
+
+
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'File',
+              items: [{
+                      label: 'New', 
+                      icon: 'pi pi-fw pi-plus',
+                      items: [
+                          {label: 'Project'},
+                          {label: 'Other'},
+                      ]
+                  },
+                  {label: 'Open'},
+                  {label: 'Quit'}
+              ]
+          },
+          {
+              label: 'Edit',
+              icon: 'pi pi-fw pi-pencil',
+              items: [
+                  {label: 'Delete', icon: 'pi pi-fw pi-trash'},
+                  {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
+              ]
+          }
+      ];
+  }
 }
